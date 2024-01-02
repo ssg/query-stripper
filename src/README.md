9d62d58ee7edde8f74bf0a0612e6874b641cec94
Sedat Kapanoglu <sedat@kapanoglu.com>
2024-01-01 19:28:53 -0800

    make it working

---
+++ src/README.md
@@ -0,0 +1,10 @@
+# remove_instance_id
+
+i found this piece of code i wrote in one of my drives. it tries to remove 
+"instanceId" from a query string. maybe it's part of my attempt to fast iterate 
+on processing device identifiers in Win32 API output. 
+
+it was still buggy and non-generic, so i rewrote it, and shortened it while
+making it expressive. it was fantastic. 
+
+i'm just archiving this. 
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    println!("{}", remove_query_string("", "instanceId"));
+    println!("{}", remove_query_string("instanceId=1234", "instanceId"));
+    println!(
+        "{}",
+        remove_query_string("instanceId=1234&x=5&y=6", "instanceId")
+    );
+    println!(
+        "{}",
+        remove_query_string("a=5&instanceId=1234&x=5&y=6", "instanceId")
+    );
+    println!(
+        "{}",
+        remove_query_string("a=5&x=5&y=6&instanceId=1234", "instanceId")
+    );
+    println!(
+        "{}",
+        remove_query_string("a=5&x=5&y=6&binstanceId=1234", "instanceId")
+    );
+}
+
+pub fn remove_query_string(query_str: &str, key: &str) -> String {
+    const SEPARATOR: &str = "&";
+
+    query_str
+        .split(SEPARATOR)
+        .filter(|p| !p.starts_with(&format!("{}=", key)))
+        .collect::<Vec<&str>>()
+        .join(SEPARATOR)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("", "")]
+    #[test_case("instanceId=1234", "")]
+    #[test_case("instanceId=1234&x=5&y=6", "x=5&y=6")]
+    #[test_case("a=5&instanceId=1234&x=5&y=6", "a=5&x=5&y=6")]
+    #[test_case("a=5&x=5&y=6&instanceId=1234", "a=5&x=5&y=6")]
+    #[test_case("a=5&x=5&y=6&binstanceId=1234", "a=5&x=5&y=6&binstanceId=1234")]
+    #[test_case("a=5&x=5&y=6&instanceId=1234&instanceId=5678", "a=5&x=5&y=6")]
+    #[test_case(
+        "instanceId=1234&a=5&instanceId=5678&x=5&instanceId=5678&y=6&instanceId=9abc",
+        "a=5&x=5&y=6"
+    )]
+    fn test_add(input: &str, output: &str) {
+        assert_eq!(output, remove_query_string(input, "instanceId"));
+    }
+}
